b824d9e0edec871ac8d07e137aafe5a3e035ccff
Dmitrii Doroshenko <lendmid@gmail.com>
2024-05-21 21:13:51 +0300

    fix redirection

--- src/main.tsx
+++ src/main.tsx
@@ -2,27 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { RouterProvider, createBrowserRouter, redirect } from "react-router-dom";
+import { BrowserRouter, Route, RouteComponentProps } from "react-router-dom";
 
-const {VITE_API_URL} = import.meta.env
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/:hash",
-    loader: async ({params}) => {
-      const clientUrl = "/" + params.hash
-      const apiUrl = new URL(clientUrl, VITE_API_URL)
-      throw redirect(apiUrl.href)
-    }
-  },
-]);
+const { VITE_API_URL } = import.meta.env;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Route exact path="/">
+        <App />
+      </Route>
+
+      <Route
+        exact
+        path="/:hash"
+        component={(routeProps: RouteComponentProps) => {
+          window.location.href = VITE_API_URL + routeProps.match.url;
+          return null;
+        }}
+      />
+    </BrowserRouter>
   </React.StrictMode>,
 );
